5933ff4963e2ebc34a117e5b36fef379671b0fb4
Roberto Costa Lima <robertogurgel@gmail.com>
2023-01-23 19:33:30 -0300

    Criado a grid base para TopBar

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,23 @@
+import {Grid} from "@mui/material";
+
+import "./styles.scss";
+
 export default function Header() {
     return (
-        <div>
-            Componente do Header
+        <div className="header">
+            <Grid container>
+                <Grid item xs={3}>
+                    Logo da Digital
+                </Grid>
+
+                <Grid item xs={5}>
+                    Campo de busca
+                </Grid>
+
+                <Grid item xs={4}>
+                    Botões
+                </Grid>
+            </Grid>
         </div>
     );
 }
