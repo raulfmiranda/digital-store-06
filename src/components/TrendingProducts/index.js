2826cfbab7f4c360a40c5bf5bf279ffdd3797018
Raul Figueira <raulfmiranda@gmail.com>
2023-01-25 16:28:46 -0300

    layout almost done

--- src/components/TrendingProducts/index.js
+++ src/components/TrendingProducts/index.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
-import Card from '@material-ui/core/Card';
-import CardMedia from '@material-ui/core/CardMedia';
-import CardContent from '@material-ui/core/CardContent';
-import Typography from '@material-ui/core/Typography';
 
 import "./styles.scss";
 
@@ -24,76 +20,91 @@ const ProductGrid = () => {
         {
             name: "K-Swiss V8 - Masculino",
             type: "Tênis",
-            salesPrice: "R$ 100",
-            price: "R$ 150",
-            imageURL: imgURL
+            salesPrice: "$100",
+            price: "$200",
+            imageURL: imgURL,
+            promo: '30% OFF'
         },
         {
             name: "K-Swiss V8 - Masculino",
             type: "Tênis",
-            salesPrice: "R$ 100",
-            price: "R$ 150",
-            imageURL: imgURL
+            salesPrice: "$100",
+            price: "$200",
+            imageURL: imgURL,
+            promo: '30% OFF'
         },
         {
             name: "K-Swiss V8 - Masculino",
             type: "Tênis",
-            salesPrice: "R$ 100",
-            price: "R$ 150",
-            imageURL: imgURL
+            salesPrice: "$100",
+            price: "$200",
+            imageURL: imgURL,
+            promo: ''
         },
         {
             name: "K-Swiss V8 - Masculino",
             type: "Tênis",
-            salesPrice: "R$ 100",
-            price: "R$ 150",
-            imageURL: imgURL
+            salesPrice: "$100",
+            price: "$200",
+            imageURL: imgURL,
+            promo: ''
         },{
             name: "K-Swiss V8 - Masculino",
             type: "Tênis",
-            salesPrice: "R$ 100",
-            price: "R$ 150",
-            imageURL: imgURL
+            salesPrice: "$100",
+            price: "$200",
+            imageURL: imgURL,
+            promo: ''
         },
         {
             name: "K-Swiss V8 - Masculino",
             type: "Tênis",
-            salesPrice: "R$ 100",
-            price: "R$ 150",
-            imageURL: imgURL
+            salesPrice: "$100",
+            price: "$200",
+            imageURL: imgURL,
+            promo: ''
         },
         {
             name: "K-Swiss V8 - Masculino",
             type: "Tênis",
-            salesPrice: "R$ 100",
-            price: "R$ 150",
-            imageURL: imgURL
+            salesPrice: "$100",
+            price: "$200",
+            imageURL: imgURL,
+            promo: ''
         },
         {
             name: "K-Swiss V8 - Masculino",
             type: "Tênis",
-            salesPrice: "R$ 100",
-            price: "R$ 150",
-            imageURL: imgURL
+            salesPrice: "$100",
+            price: "$200",
+            imageURL: imgURL,
+            promo: ''
         }
     ];
 
     return (
-        <Grid container justifyContent="center" spacing={2}>
-            {products.map((product) => (
-                <Grid item>
-                    <item>
-                    <img className='product-img' src={product.imageURL}/>
-                    <p className='product-type'>{product.type}</p>
-                    <p className='product-name'>{product.name}</p>
-                    <p className='product-prices'>
-                        <span className='product-price'>{product.price}</span>
-                        <span className='product-salesPrice'>{product.salesPrice}</span>
-                    </p>
-                    </item>
-                </Grid>
-            ))}
-        </Grid>
+        <section className='trending-products'>
+            <div className='gridtop'>
+                <span><b>Produtos em alta</b></span>
+                <span className='see-all'>Ver todos &rarr;</span>
+            </div>
+            <Grid container spacing={0.5}>
+                {products.map((product) => (
+                    <Grid item xs={12} sm={4} md={3} className='aaa'>
+                        <div className='product-promo'>
+                            <p className='product-promo-content'>{product.promo}</p>
+                        </div>
+                        <img className='product-img' src={product.imageURL}/>
+                        <p className='product-type'>{product.type}</p>
+                        <p className='product-name'>{product.name}</p>
+                        <p className='product-prices'>
+                            <span className='product-price'>{product.price}</span>
+                            <span className='product-salesPrice'><b>{product.salesPrice}</b></span>
+                        </p>
+                    </Grid>
+                ))}
+            </Grid>
+        </section>
     );
 };
 
