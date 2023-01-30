b4d3a39061c4b1eaa7e8cb0ba2c2737371f8e8ea
Raul <raulfmiranda@gmail.com>
2023-01-30 20:32:40 -0300

    layout fix

--- src/components/TrendingProducts/index.js
+++ src/components/TrendingProducts/index.js
@@ -1,17 +1,8 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
-
+import Paper from '@material-ui/core/Paper';
+import productimg from './assets/productimg.png';
 import "./styles.scss";
-
-const useStyles = makeStyles({
-    card: {
-        maxWidth: 292,
-    },
-    media: {
-        height: 140,
-    },
-});
 
 const ProductGrid = () => {
     const imgURL = "https://raw.githubusercontent.com/raulfmiranda/digital-store-06/main/src/components/TrendingProducts/assets/productimg.jpg";
@@ -88,13 +79,18 @@ const ProductGrid = () => {
                 <span><b>Produtos em alta</b></span>
                 <span className='see-all'>Ver todos &rarr;</span>
             </div>
-            <Grid container spacing={0.5}>
+            <Grid container spacing={0.5} justifyContent="center">
                 {products.map((product) => (
                     <Grid item xs={12} sm={4} md={3} className='aaa'>
-                        <div className='product-promo'>
-                            <p className='product-promo-content'>{product.promo}</p>
-                        </div>
-                        <img className='product-img' src={product.imageURL}/>
+                        <Paper elevation={3}>
+                            <div className='product-promo'>
+                                {
+                                    product.promo &&
+                                    <p className='product-promo-content'>{product.promo}</p>
+                                }
+                            </div>
+                            <img className='product-img' src={productimg}/>
+                        </Paper>
                         <p className='product-type'>{product.type}</p>
                         <p className='product-name'>{product.name}</p>
                         <p className='product-prices'>
